2c3fa27269c1b7c192386ecfa796047a7fba9be2
Danex2 <khadane.miller@gmail.com>
2018-09-21 19:44:17 -0400

    added discord.py

--- bot.py
+++ bot.py
@@ -1,13 +1,28 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+import discord
+import asyncio
 
-req = Request('https://pcpartpicker.com/list/BgK49J',
-              headers={'User-Agent': 'Mozilla/5.0'})
-url = urlopen(req)
-content = url.read()
-soup = BeautifulSoup(content, "html.parser")
-i = soup.findAll("td", {'class': ["component-type", "component-name"]})
-result = []
 
-for c in i:
-    print(c.get_text().strip('\n'))
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Running ' + client.user.name)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('~build'):
+        req = Request('https://pcpartpicker.com/list/BgK49J',
+                      headers={'User-Agent': 'Mozilla/5.0'})
+        url = urlopen(req)
+        content = url.read()
+        soup = BeautifulSoup(content, "html.parser")
+        i = soup.findAll("td", {'class': ["component-type", "component-name"]})
+        output = []
+        for c in i:
+            output.append(c.get_text().strip('\n'))
+        print(", ".join(output))
+client.run('NDkyODAxMTI3Mzk4NjM3NTY4.DobsfQ.yEeJaWUk75mcLhLnUnI6LAsfIGg')
