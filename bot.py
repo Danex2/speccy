c35bb6f6901c01acd070f00abc2535c3355f8a1f
Danex2 <khadane.miller@gmail.com>
2018-09-22 14:38:12 -0400

    displaying builds now works

--- bot.py
+++ bot.py
@@ -9,20 +9,23 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('Running ' + client.user.name)
+    print('Running ' + client.user.name + ' ' + client.user.id)
 
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('~build'):
+    if message.content.startswith('!build'):
         req = Request('https://pcpartpicker.com/list/BgK49J',
                       headers={'User-Agent': 'Mozilla/5.0'})
         url = urlopen(req)
         content = url.read()
+        embed = discord.Embed(title="", color=0xcc1df1)
         soup = BeautifulSoup(content, "html.parser")
         i = soup.findAll("td", {'class': ["component-type", "component-name"]})
         output = []
-        for c in i:
-            output.append(c.get_text().strip('\n'))
-        print(", ".join(output))
+        for x in i:
+            output.append(x.get_text().strip("\n"))
+        embed.add_field(name="Dane's build", value='\n'.join(output))
+        await client.send_message(message.channel, embed=embed)
+
 client.run('NDkyODAxMTI3Mzk4NjM3NTY4.DobsfQ.yEeJaWUk75mcLhLnUnI6LAsfIGg')
